293f86511ccc24fe53324ca6e646fe5a04031102
keremaktug <kerem.aktug@relimetrics.com>
2026-07-24 17:56:10 +0300

    ...

---
+++ Minimap/main.cpp
@@ -0,0 +1,50 @@
+#include <QApplication>
+#include <QIcon>
+#include <QMainWindow>
+#include <QMetaObject>
+#include <QString>
+#include <QUrl>
+
+#include "SampleSupport.h"
+#include "Viewer/GisViewer.h"
+#include "Controls/MiniMap/GisMiniMap.h"
+
+using namespace GeoKernel::Viewer;
+using namespace GeoKernel::Viewer::Controls::Minimap;
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    QApplication::setWindowIcon(QIcon(QStringLiteral(":/icons/geokernel.ico")));
+    QMainWindow window;
+    window.resize(1200, 800);
+    window.setWindowTitle(QStringLiteral("Minimap"));
+
+    auto* viewer = new GisViewer(&window);
+    viewer->setActiveTool(GisViewerTool::Pan);
+    window.setCentralWidget(viewer);
+
+    auto* miniMap = new GisMiniMap(viewer);
+    miniMap->setViewer(viewer);
+    miniMap->resize(220, 150);
+    miniMap->setAnchor(GisMiniMapAnchor::TopRight);
+    miniMap->raise();
+    miniMap->show();
+
+    window.show();
+
+    QMetaObject::invokeMethod(&window, [&window, viewer]
+    {
+        const QString worldLayerPath = ensureSampleFile(
+            QUrl(QStringLiteral("https://github.com/geokernel-io/GeoKernel.SampleData/releases/download/v1/world_4326.zip")),
+            QStringLiteral("world_4326.zip"),
+            QStringLiteral("world_4326"),
+            QStringLiteral("world_4326.shp"),
+            &window);
+
+        if (!worldLayerPath.isEmpty() && loadLayer(*viewer, worldLayerPath, &window))
+            viewer->fullExtent();
+    });
+
+    return app.exec();
+}
